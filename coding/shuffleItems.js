976a7b1ef96e51dabb1bf2a4cf737c46e768b9f0
Asad Aziz <asadazizjs@gmail.com>
2022-07-21 19:40:26 +0500

    added shuffle items function

---
+++ coding/shuffleItems.js
@@ -0,0 +1,12 @@
+// write a function which implements shuffle
+
+const shuffleItems = (items) => {
+  return items
+    .map((item) => ({
+      sort: Math.random(),
+      value: item,
+    }))
+    .sort((item1, item2) => item1.sort - item2.sort)
+    .map((a) => a.value);
+};
+// console.log(shuffleItems([1, 2]));
